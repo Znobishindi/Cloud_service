f922e6b9519fe8dbd69b126fcf26a4d45d02bc32
Denis Znobishin <znobishindi@yandex.ru>
2023-06-29 23:01:56 +0300

    Money Transfer Service

---
+++ src/main/java/com/example/cloud_storage_v3/conf/SecurityConf.java
@@ -0,0 +1,79 @@
+package com.example.cloud_storage_v3.conf;
+
+
+import com.example.cloud_storage_v3.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@EnableWebMvc
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConf implements WebMvcConfigurer {
+
+    private final JwtFilter jwtFilter;
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()
+                .formLogin().disable()
+                .csrf().disable()
+                .cors().and()
+                .logout().disable()
+                .authorizeRequests(
+                        authz -> authz
+                                .requestMatchers("/", "/favicon.ico").permitAll()
+                                .requestMatchers(AUTH_WHITELIST).permitAll() // whitelist URL permitted for swagger
+                                .requestMatchers("/login", "/users/create").permitAll()
+                                .anyRequest().authenticated()
+                                .and()
+                                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                )
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:8090")
+                .allowedMethods("*");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+}
